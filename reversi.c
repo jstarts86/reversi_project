2ebf733504ef8fe78042da26956c19554f40970b
jstarts86 <67910821+jstarts86@users.noreply.github.com>
2023-06-19 01:53:26 +0900

    Update reversi.c
    Added a loop.
     Loop iterates over all possible directions in the game of Reversi. This will be used to check for legal moves in each direction from the selected board cell."

--- reversi.c
+++ reversi.c
@@ -40,4 +40,8 @@ int is_valid_move(int board[8][8], int y, int x, int player) {
         return 0;
     }
 
+    int opponent = 3 - player;
+    int directions[8][2] = {{0, -1}, {0, 1}, {-1, 0}, {1, 0}, {-1, -1}, {1, -1}, {-1, 1}, {1, 1}};
+        for (int i = 0; i < 8; i++) {
+        }
 }
