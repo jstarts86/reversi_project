1bdac6f8774a63feb6b1748f1f9237ff735233bd
JungHyunjun <103713434+ark1210@users.noreply.github.com>
2023-06-19 01:18:12 +0900

    reversi.c
    This commit initializes the function 'flip_pieces' which will later
    be responsible for flipping opponent pieces in the Reversi game.
    
    The function is currently an empty shell and its implementation will 
    be added in the future commits. This function takes as parameters 
    a 8x8 game board and the coordinates (y, x) of the piece to be placed, 
    along with the player number.
    
    - The function will return the number of pieces flipped
    - It will change the game board state

---
+++ reversi.c
@@ -0,0 +1,10 @@
+#include <ncurses.h>
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <string.h> 
+#include <sys/socket.h> 
+#include <netinet/in.h> 
+
+int flip_pieces(int board[8][8], int y, int x, int player) {
+ 
+}
