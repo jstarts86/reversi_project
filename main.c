f02539d4ae52bcdfcc30ead72d3945ad40c19a32
jstarts86 <67910821+jstarts86@users.noreply.github.com>
2023-06-19 13:28:23 +0900

    Update main.c
    Added the header files

--- main.c
+++ main.c
@@ -1,4 +1,10 @@
 #include "reversi.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+
 
 int main(int argc, char* argv[]) {
     if (strcmp(argv[1], "server") == 0) {
