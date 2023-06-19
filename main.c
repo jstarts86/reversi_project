f3b44e9522cf858403135f8b1b3ca7b237bdcea7
jstarts86 <67910821+jstarts86@users.noreply.github.com>
2023-06-19 13:12:52 +0900

    Create main.c
    Main method of reversi program.

---
+++ main.c
@@ -0,0 +1,28 @@
+#include "reversi.h"
+
+int main(int argc, char* argv[]) {
+    if (strcmp(argv[1], "server") == 0) {
+        if (argc != 3) {
+            fprintf(stderr, "Usage: %s server [port]\n", argv[0]);
+            return 1;
+        }
+        int port = atoi(argv[2]);
+        int socket = listen_at_port(port);
+        run_reversi(socket, IS_SERVER);
+    } else if (strcmp(argv[1], "client") == 0) {
+        if (argc != 4) {
+            fprintf(stderr, "Usage: %s client [ip address] [port]\n", argv[0]);
+            return 1;
+        }
+        char* ip = argv[2];
+        int port = atoi(argv[3]);
+        int socket = connect_ipaddr_port(ip, port);
+        run_reversi(socket, IS_CLIENT);
+        shutdown(socket, SHUT_RDWR);
+    } else {
+        fprintf(stderr, "Invalid argument: %s. Expect 'server' or 'client'\n", argv[1]);
+        return 1;
+    }
+
+    return 0;
+}
