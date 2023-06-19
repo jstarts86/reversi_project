fd541df808d11db8efe1ef3df8dff64f727c2467
jstarts86 <67910821+jstarts86@users.noreply.github.com>
2023-06-19 13:11:02 +0900

    Update reversi.h
    Defines server and client roles.
    Sets the functions for the Reversi game, including board manipulation, validation of moves.

--- reversi.h
+++ reversi.h
@@ -1,4 +1,17 @@
 #ifndef REVERSI_H
 #define REVERSI_H
 
+#define IS_SERVER 0
+#define IS_CLIENT 1
+
 int flip_pieces(int board[8][8], int y, int x, int player);
+int is_valid_move(int board[8][8], int y, int x, int player);
+int can_make_move(int board[8][8], int player);
+void display_board(int board[8][8], int currentPlayer);
+void count_pieces(int board[8][8], int* p1_pieces, int* p2_pieces);
+int listen_at_port(int portnum);
+void chat(int conn_fd);
+int connect_ipaddr_port(const char* ip, int port);
+void run_reversi(int socket, int role);
+
+#endif /* REVERSI_H */
